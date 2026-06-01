3108226a98b890fec7bb9f04ffa652eaf14f7994
Mahmoud Salheen <131267754+Salheen10@users.noreply.github.com>
2026-06-02 01:01:22 +0300

    Rebrand the About page texts from SuiteCRM to Badirra CRM globally

---
+++ custom/modules/Home/language/ar.lang.php
@@ -0,0 +1,9 @@
+<?php
+$mod_strings['LBL_CONTRIBUTOR_SUITECRM'] = 'Badirra CRM - نظام علاقات العملاء للمؤسسات حول العالم';
+$mod_strings['LBL_ABOUT_SUITE'] = 'عن Badirra CRM';
+$mod_strings['LBL_ABOUT_SUITE_2'] = 'يتم نشر Badirra CRM بموجب ترخيص مفتوح المصدر - AGPLv3';
+$mod_strings['LBL_ABOUT_SUITE_4'] = 'كل أكواد Badirra CRM المُدارة والمُطورة بواسطة المشروع سيتم إصدارها كمصدر مفتوح - AGPLv3';
+$mod_strings['LBL_ABOUT_SUITE_5'] = 'دعم Badirra CRM متاح في خيارات مجانية ومدفوعة';
+$mod_strings['LBL_MAINTAINER_SUITECRM'] = 'تمت كتابة وصيانة Badirra CRM بواسطة Badirra CRM Ltd';
+$mod_strings['LBL_SUITE_PARTNERS'] = 'لدينا شركاء Badirra CRM مخلصين ومتحمسين للمصدر المفتوح. لعرض قائمة الشركاء الكاملة، راجع موقعنا.';
+$mod_strings['LBL_LANGUAGE_ABOUT'] = 'عن ترجمات Badirra CRM';
---
+++ custom/modules/Home/language/en_us.lang.php
@@ -0,0 +1,9 @@
+<?php
+$mod_strings['LBL_CONTRIBUTOR_SUITECRM'] = 'Badirra CRM - Enterprise CRM for the world';
+$mod_strings['LBL_ABOUT_SUITE'] = 'About Badirra CRM';
+$mod_strings['LBL_ABOUT_SUITE_2'] = 'Badirra CRM is published under an open source licence - AGPLv3';
+$mod_strings['LBL_ABOUT_SUITE_4'] = 'All Badirra CRM code managed and developed by the project will be released as open source - AGPLv3';
+$mod_strings['LBL_ABOUT_SUITE_5'] = 'Badirra CRM support is available in both free and paid-for options';
+$mod_strings['LBL_MAINTAINER_SUITECRM'] = 'Badirra CRM is written and maintained by Badirra CRM Ltd';
+$mod_strings['LBL_SUITE_PARTNERS'] = 'We have loyal Badirra CRM partners who are passionate about open source. To view our full partner list, see our website.';
+$mod_strings['LBL_LANGUAGE_ABOUT'] = 'About Badirra CRM Translations';
