0b6dab59084ee16e00498fa0a84f980c33863643
Mahmoud Salheen <131267754+Salheen10@users.noreply.github.com>
2026-06-01 22:49:42 +0300

    Fix Docker session timeout issue and set default language to English
    - Set verify_client_ip to false to prevent random logouts (Session timeout) when running behind Docker/proxy due to IP mismatch
    - Set default_language to en_us so English is selected by default on the login screen

--- config_override.php
+++ config_override.php
@@ -1,3 +1,4 @@
 <?php
-$sugar_config['default_language'] = 'ar';
+$sugar_config['default_language'] = 'en_us';
 $sugar_config['languages']['ar'] = 'العربية';
+$sugar_config['verify_client_ip'] = false;
